eb508373edd2f99a6d6bb5935f77190ab4757bef
Brendan Gerardi <brendan.gerardi@gmail.com>
2018-01-21 15:17:38 -0600

    added triggers for buttons on page

--- public/client.js
+++ public/client.js
@@ -84,3 +84,233 @@ $('.registerButton').click(function (event) {
     $(".recipeInfoScreen").hide();
     $(".createRecipeScreen").hide();
 });
+
+//nav bar options//
+$('.jsMyLibrary').click(function (event) {
+    event.preventDefault();
+    $(".introScreen").hide();
+    $(".signInScreen").hide();
+    $(".createUsernameScreen").hide();
+    $(".dummyAccountScreen").hide();
+    $(".homeScreen").show();
+    $(".searchScreen").hide();
+    $(".chosenFail").hide();
+    $(".recipeInfoScreen").hide();
+    $(".createRecipeScreen").hide();
+});
+
+$('.jsCreateRecipe').click(function (event) {
+    event.preventDefault();
+    $(".introScreen").hide();
+    $(".signInScreen").hide();
+    $(".createUsernameScreen").hide();
+    $(".dummyAccountScreen").hide();
+    $(".homeScreen").hide();
+    $(".searchScreen").hide();
+    $(".chosenFail").hide();
+    $(".recipeInfoScreen").hide();
+    $(".createRecipeScreen").show();
+});
+
+$('.jsLogout').click(function (event) {
+    event.preventDefault();
+    $(".introScreen").hide();
+    $(".signInScreen").show();
+    $(".createUsernameScreen").hide();
+    $(".dummyAccountScreen").hide();
+    $(".homeScreen").hide();
+    $(".searchScreen").hide();
+    $(".chosenFail").hide();
+    $(".recipeInfoScreen").hide();
+    $(".createRecipeScreen").hide();
+});
+//end of nav bar options
+
+//recipe img --> recipe info//
+$('.recipeLink').click(function (event) {
+    event.preventDefault();
+    $(".introScreen").hide();
+    $(".signInScreen").hide();
+    $(".createUsernameScreen").hide();
+    $(".dummyAccountScreen").hide();
+    $(".homeScreen").hide();
+    $(".searchScreen").hide();
+    $(".chosenFail").hide();
+    $(".recipeInfoScreen").show();
+    $(".createRecipeScreen").hide();
+});
+
+//search bar button --> success scenario//
+$('.searchSubmit').click(function (event) {
+    event.preventDefault();
+    $(".introScreen").hide();
+    $(".signInScreen").hide();
+    $(".createUsernameScreen").hide();
+    $(".dummyAccountScreen").hide();
+    $(".homeScreen").hide();
+    $(".searchScreen").show();
+    $(".chosenFail").hide();
+    $(".recipeInfoScreen").hide();
+    $(".createRecipeScreen").hide();
+});
+
+//search bar button --> fail scenario//
+$('.searchSubmit').click(function (event) {
+    event.preventDefault();
+    $(".introScreen").hide();
+    $(".signInScreen").hide();
+    $(".createUsernameScreen").hide();
+    $(".dummyAccountScreen").hide();
+    $(".homeScreen").hide();
+    $(".searchScreen").hide();
+    $(".chosenFail").show();
+    $(".recipeInfoScreen").hide();
+    $(".createRecipeScreen").hide();
+});
+
+//recipe info screen(modify button)//
+$('#modifyAnchor').click(function (event) {
+    event.preventDefault();
+    $(".introScreen").hide();
+    $(".signInScreen").hide();
+    $(".createUsernameScreen").hide();
+    $(".dummyAccountScreen").hide();
+    $(".homeScreen").hide();
+    $(".searchScreen").hide();
+    $(".chosenFail").hide();
+    $(".recipeInfoScreen").show();
+    $(".createRecipeScreen").hide();
+    $(".ingredientsContainer").show();
+    $(".modsList").show();
+
+});
+
+//recipe info screen(directions button-going to a different url)// ////////////////////////////////////need help///////////////////////////////////
+$('#directionsAnchor').click(function (event) {
+    event.preventDefault();
+    $(".introScreen").hide();
+    $(".signInScreen").hide();
+    $(".createUsernameScreen").hide();
+    $(".dummyAccountScreen").hide();
+    $(".homeScreen").hide();
+    $(".searchScreen").hide();
+    $(".chosenFail").hide();
+    $(".recipeInfoScreen").hide();
+    $(".createRecipeScreen").hide();
+});
+
+//add ingredient button//
+$('.jsAddButton').click(function (event) {
+    event.preventDefault();
+    $(".introScreen").hide();
+    $(".signInScreen").hide();
+    $(".createUsernameScreen").hide();
+    $(".dummyAccountScreen").hide();
+    $(".homeScreen").hide();
+    $(".searchScreen").hide();
+    $(".chosenFail").hide();
+    $(".recipeInfoScreen").show();
+    $(".createRecipeScreen").hide();
+    $(".ingredientsContainer").hide();
+    $(".modsList").show();
+});
+
+//delete ingredient button//
+$('.jsDeleteButton').click(function (event) {
+    event.preventDefault();
+    $(".introScreen").hide();
+    $(".signInScreen").hide();
+    $(".createUsernameScreen").hide();
+    $(".dummyAccountScreen").hide();
+    $(".homeScreen").hide();
+    $(".searchScreen").hide();
+    $(".chosenFail").hide();
+    $(".recipeInfoScreen").show();
+    $(".createRecipeScreen").hide();
+    $(".ingredientsContainer").hide();
+    $(".modsList").show();
+});
+
+//save recipe button//
+$('.').click(function (event) {
+    event.preventDefault();
+    $(".introScreen").hide();
+    $(".signInScreen").show();
+    $(".createUsernameScreen").hide();
+    $(".dummyAccountScreen").hide();
+    $(".homeScreen").hide();
+    $(".searchScreen").hide();
+    $(".chosenFail").hide();
+    $(".recipeInfoScreen").hide();
+    $(".createRecipeScreen").hide();
+});
+
+//delete recipe button//
+$('.').click(function (event) {
+    event.preventDefault();
+    $(".introScreen").hide();
+    $(".signInScreen").show();
+    $(".createUsernameScreen").hide();
+    $(".dummyAccountScreen").hide();
+    $(".homeScreen").hide();
+    $(".searchScreen").hide();
+    $(".chosenFail").hide();
+    $(".recipeInfoScreen").hide();
+    $(".createRecipeScreen").hide();
+});
+
+//save created recipe button//
+$('.').click(function (event) {
+    event.preventDefault();
+    $(".introScreen").hide();
+    $(".signInScreen").show();
+    $(".createUsernameScreen").hide();
+    $(".dummyAccountScreen").hide();
+    $(".homeScreen").hide();
+    $(".searchScreen").hide();
+    $(".chosenFail").hide();
+    $(".recipeInfoScreen").hide();
+    $(".createRecipeScreen").hide();
+});
+
+//cancel created recipe button//
+$('.').click(function (event) {
+    event.preventDefault();
+    $(".introScreen").hide();
+    $(".signInScreen").show();
+    $(".createUsernameScreen").hide();
+    $(".dummyAccountScreen").hide();
+    $(".homeScreen").hide();
+    $(".searchScreen").hide();
+    $(".chosenFail").hide();
+    $(".recipeInfoScreen").hide();
+    $(".createRecipeScreen").hide();
+});
+
+//footer button????????//
+$('.').click(function (event) {
+    event.preventDefault();
+    $(".introScreen").hide();
+    $(".signInScreen").show();
+    $(".createUsernameScreen").hide();
+    $(".dummyAccountScreen").hide();
+    $(".homeScreen").hide();
+    $(".searchScreen").hide();
+    $(".chosenFail").hide();
+    $(".recipeInfoScreen").hide();
+    $(".createRecipeScreen").hide();
+});
+
+//add ingredient button//
+$('.').click(function (event) {
+    event.preventDefault();
+    $(".introScreen").hide();
+    $(".signInScreen").show();
+    $(".createUsernameScreen").hide();
+    $(".dummyAccountScreen").hide();
+    $(".homeScreen").hide();
+    $(".searchScreen").hide();
+    $(".chosenFail").hide();
+    $(".recipeInfoScreen").hide();
+    $(".createRecipeScreen").hide();
+});
